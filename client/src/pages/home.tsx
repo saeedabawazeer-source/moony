ce6c4cf61c28b1d81fb8a8e0091c22feef2a5124
Saeed Bawazer <saeed@Saeeds-MacBook-Pro.local>
2026-06-30 19:27:28 -0700

    fix(ui): lower canvas wave, slow animation, and restore starfish opacities

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -116,7 +116,7 @@ export default function Home() {
         <section className="snap-slide relative overflow-hidden bg-[#fef8e1]">
           <Header />
           
-          <div className="absolute inset-x-0 bottom-0 top-[20%] z-0 pointer-events-none">
+          <div className="absolute inset-x-0 bottom-0 top-[35%] z-0 pointer-events-none">
             <CanvasWave />
           </div>
 
@@ -173,9 +173,9 @@ export default function Home() {
           <div className="absolute inset-0 w-full h-full pointer-events-none z-50 overflow-hidden">
             <motion.img src="/images/starfish-coral.png" className="absolute top-[10%] right-[5%] w-12 lg:w-20 pointer-events-none" animate={{ y: [0, -20, 0], rotate: [0, 15, 0] }} transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }} />
             <motion.img src="/images/starfish-teal.png" className="absolute top-[85%] left-[5%] w-16 lg:w-24 pointer-events-none" animate={{ y: [0, 25, 0], rotate: [0, -10, 0] }} transition={{ duration: 8, repeat: Infinity, ease: "easeInOut" }} />
-            <motion.img src="/images/starfish-coral.png" className="absolute top-[45%] left-[2%] w-10 lg:w-16 pointer-events-none opacity-40" animate={{ x: [0, 15, 0], rotate: [0, 360] }} transition={{ x: { duration: 10, repeat: Infinity, ease: "easeInOut" }, rotate: { duration: 40, repeat: Infinity, ease: "linear" } }} />
-            <motion.img src="/images/starfish-teal.png" className="absolute top-[15%] left-[10%] w-8 lg:w-12 pointer-events-none opacity-30" animate={{ scale: [1, 1.2, 1], rotate: [0, 90, 0] }} transition={{ duration: 5, repeat: Infinity }} />
-            <motion.img src="/images/starfish-coral.png" className="absolute top-[80%] right-[8%] w-14 lg:w-22 pointer-events-none opacity-20" animate={{ x: [0, -30, 0], y: [0, 15, 0] }} transition={{ duration: 9, repeat: Infinity }} />
+            <motion.img src="/images/starfish-coral.png" className="absolute top-[45%] left-[2%] w-10 lg:w-16 pointer-events-none" animate={{ x: [0, 15, 0], rotate: [0, 360] }} transition={{ x: { duration: 10, repeat: Infinity, ease: "easeInOut" }, rotate: { duration: 40, repeat: Infinity, ease: "linear" } }} />
+            <motion.img src="/images/starfish-teal.png" className="absolute top-[15%] left-[10%] w-8 lg:w-12 pointer-events-none" animate={{ scale: [1, 1.2, 1], rotate: [0, 90, 0] }} transition={{ duration: 5, repeat: Infinity }} />
+            <motion.img src="/images/starfish-coral.png" className="absolute top-[80%] right-[8%] w-14 lg:w-22 pointer-events-none" animate={{ x: [0, -30, 0], y: [0, 15, 0] }} transition={{ duration: 9, repeat: Infinity }} />
           </div>
         </section>
 
